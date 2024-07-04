69358240b5f0d31955745d72df888befd121b0d6
codingexamples <andreas@witte.me>
2024-07-04 14:16:22 +0200

    added command line args

--- index.js
+++ index.js
@@ -1,33 +1,73 @@
 var http = require('node:http');
 
-// Access the command line arguments
-const cmdArgs = process.argv.slice(2); // Slice to remove the first two elements
+function getFormattedTimestamp() {
+    const date = new Date();
 
-// Process the arguments
-// cmdArgs.forEach((arg, index) => {
-//     console.log(`Argument ${index}: ${arg}`);
-// });
+    const year = date.getFullYear();
+    const month = String(date.getMonth() + 1).padStart(2, '0'); // Months are zero-based
+    const day = String(date.getDate()).padStart(2, '0');
 
-// Example of how to handle specific options
-const cmdOptions = {
-    name: null,
-    age: null
+    const hours = String(date.getHours()).padStart(2, '0');
+    const minutes = String(date.getMinutes()).padStart(2, '0');
+    const seconds = String(date.getSeconds()).padStart(2, '0');
+    const ms = String(date.getMilliseconds()).padStart(3, '0');
+
+    return `${year}-${month}-${day} ${hours}:${minutes}:${seconds}.${ms}`;
+}
+
+// Slice to remove the first two elements
+const cmdArgs = process.argv.slice(2);
+const options = {
+    port: 8080,
+    hostname: "localhost",
+    contentType: 'text/html',
+    responseStatus: 200,
+    response: 'OK'
 };
-
 cmdArgs.forEach(arg => {
     const [key, value] = arg.split('=');
-    if (key === '--name') {
-        cmdOptions.name = value;
-    } else if (key === '--age') {
-        cmdOptions.age = value;
+    if (key === '--content-type') {
+        options.contentType = value;
+    } else if (key === '--response-status') {
+        let responseStatusNumber = Number(value);
+        if (isNaN(responseStatusNumber)) {
+            console.error('Invalid number in command line option --response-status.');
+            process.exit(1);
+        } else {
+            options.responseStatus = responseStatusNumber;
+        }
+    } else if (key === '--response') {
+        // console.log(value);
+        if (options.contentType == "application/json") {
+            let jsonObjectFromValue;
+            try {
+                jsonObjectFromValue = JSON.parse(value);
+                options.response = jsonObjectFromValue
+            } catch (error) {
+                console.error('Invalid JSON string in command line option --response.');
+                process.exit(1);
+            }
+        } else {
+            options.response = value;
+        }
+    } else if (key === '--port') {
+        let portNumber = Number(value);
+        if (isNaN(portNumber)) {
+            console.error('Invalid number in command line option --port.');
+            process.exit(1);
+        } else {
+            options.port = portNumber;
+        }
+    } else if (key === '--hostname') {
+        options.hostname = value;
     }
 });
 
-console.log('Parsed Options:', cmdOptions);
+console.log('Server options:', options);
 
 var requestId = 0;
 
-http.createServer(function (req, res) {
+var httpServer = http.createServer(function (req, res) {
     console.log("######## REQUEST ########");
     console.log(`Request-ID: ${requestId}`);
     console.log(`Time: ${getFormattedTimestamp()}`);
@@ -48,6 +88,7 @@ http.createServer(function (req, res) {
 
     req.on('end', () => {
         // Log the complete body
+        console.log("Body:")
         console.log(body);
 
         // Respond to the client
@@ -56,8 +97,12 @@ http.createServer(function (req, res) {
         // res.end('Hello, world!\n');
         //write a response to the client
         // TODO: answer from command line option
-        res.writeHead(200, { 'Content-Type': 'text/html' });
-        res.write('Hello World!');
+        res.writeHead(options.responseStatus, { 'Content-Type': options.contentType });
+        if (options.contentType === "application/json") {
+            res.write(JSON.stringify(options.response));
+        } else {
+            res.write(options.response);
+        }
         res.end();
 
         console.log("########## END ##########");
@@ -69,27 +114,16 @@ http.createServer(function (req, res) {
         res.statusCode = 500;
         res.end('Internal Server Error\n');
 
-        console.log("########## END ##########");
-        console.log("");
+        console.log("########## END ##########\n");
     });
 
 
 
     requestId += 1;
-}).listen(8080); //the server object listens on port 8080
+})
+
+httpServer.listen(options.port, function (){
+    console.log(`Server listening at http://${options.hostname}:${options.port}/\n`);
+});
 
 
-function getFormattedTimestamp() {
-    const date = new Date();
-
-    const year = date.getFullYear();
-    const month = String(date.getMonth() + 1).padStart(2, '0'); // Months are zero-based
-    const day = String(date.getDate()).padStart(2, '0');
-
-    const hours = String(date.getHours()).padStart(2, '0');
-    const minutes = String(date.getMinutes()).padStart(2, '0');
-    const seconds = String(date.getSeconds()).padStart(2, '0');
-    const ms = String(date.getMilliseconds()).padStart(3, '0');
-
-    return `${year}-${month}-${day} ${hours}:${minutes}:${seconds}.${ms}`;
-}
